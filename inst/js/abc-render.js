a8c5518b9c1df5576329a05cdd231b5eddf55a58
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-27 16:20:18 +0100

    Reorganising

--- inst/js/abc-render.js
+++ inst/js/abc-render.js
@@ -1,4 +1,4 @@
-function load() {
+function abc_render() {
   ABCJS.renderAbc("paper", abc);
   ABCJS.renderMidi("midi", abc);
   ABCJS.renderMidi("midi-download", abc, { generateDownload: true, generateInline: false });
