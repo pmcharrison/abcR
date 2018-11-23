515f63f69723dc785d3c95d197ab8aff016e1b8d
Peter M C Harrison <pmc.harrison@gmail.com>
2018-11-23 16:06:51 +0000

    Disabling resizing

--- inst/js/abc-render.js
+++ inst/js/abc-render.js
@@ -2,8 +2,8 @@ function abc_render(id, play_midi, download_midi, staffwidth, container_style) {
   console.log("id = " + id);
   document.getElementById("abc_image_" + id).style.cssText = container_style;
   ABCJS.renderAbc("abc_image_" + id, window["abc_data_" + id], {
-    staffwidth: staffwidth,
-    responsive: 'resize'
+    staffwidth: staffwidth
+    //responsive: 'resize'
   });
   document.getElementById("abc_image_" + id).childNodes[0].style.position = 'static';
   document.getElementById("abc_image_" + id).style.cssText = container_style;
