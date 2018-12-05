be08e8dce92cb486b38b97424ea064082dab90d4
Peter M C Harrison <pmc.harrison@gmail.com>
2018-12-05 12:49:46 +0000

    Adding annotations to chord sequences

--- R/pi-chord-seq.R
+++ R/pi-chord-seq.R
@@ -1,20 +1,32 @@
 #' @export
 html_from_pi_chord_seq <- function(x,
+                                   annotate = NULL,
                                    chords_per_line = 5,
                                    staff_width = 75 * pmin(pmax(2, length(x)), 5),
                                    play_midi = FALSE,
                                    download_midi = FALSE,
                                    ...) {
   checkmate::qassert(x, "l")
+  if (!is.null(annotate)) stopifnot(is.character(annotate),
+                                    length(annotate) == length(x))
+  if (any(grepl("\\s", annotate)))
+    stop("<annotate> strings may not contain whitespace")
 
   y <- purrr::map(x, spell_pi_chord) %>% (dplyr::bind_rows)
+  if (!is.null(annotate)) y$annotate <- annotate
   by_row <- split(y, floor(seq(from = 0, length.out = nrow(y)) / chords_per_line))
   score <- purrr::map_chr(by_row, function(t) {
     treble_str <- if (all(t$treble == "x")) NULL else
       paste0("[V:1]", paste(t$treble, collapse = "|"), "|")
     bass_str <- if (all(t$bass == "x")) NULL else
       paste0("[V:2]", paste(t$bass, collapse = "|"), "|")
-    paste(c(treble_str, bass_str), collapse = "\n")
+    annotate_str <- if (!is.null(annotate))
+      paste(c("w:", t$annotate), collapse = " ")
+    paste(c(treble_str,
+            if (!is.null(treble_str)) annotate_str,
+            bass_str,
+            if (is.null(treble_str)) annotate_str
+    ), collapse = "\n")
   }) %>% paste(collapse = "\n%\n")
 
   str <- sprintf("L:1\nV:1 treble\nV:2 bass\n%s", score)
